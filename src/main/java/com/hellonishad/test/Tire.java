e02057e0f206c3a6ca3332597c13d2c0238138d2
nisadrahman <nisadrehman@gmail.com>
2020-11-28 17:12:43 +0600

    Constructor Injection

--- src/main/java/com/hellonishad/test/Tire.java
+++ src/main/java/com/hellonishad/test/Tire.java
@@ -3,6 +3,9 @@ package com.hellonishad.test;
 public class Tire {
     private String brand;
 
+    public Tire(String brand) {
+        this.brand = brand;
+    }
 
     public String getBrand() {
         return brand;
