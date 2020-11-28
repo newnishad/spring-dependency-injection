358390e79cd6476ddb0177734011a73685d4204f
nisadrahman <nisadrehman@gmail.com>
2020-11-28 17:08:12 +0600

    Bean Property

--- src/main/java/com/hellonishad/test/App.java
+++ src/main/java/com/hellonishad/test/App.java
@@ -10,7 +10,10 @@ public class App {
 	public static void main(String[] args) {
 		//SpringApplication.run(App.class, args);
 		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
-		Vehicle obj = (Vehicle) context.getBean("bike");
-		obj.drive();
+//		Vehicle obj = (Vehicle) context.getBean("bike");
+//		obj.drive();
+
+		Tire tire = (Tire)context.getBean("tire");
+		System.out.println(tire);
 	}
 }
